1723c2bf590d7010f0d70f2a72f4c87409635ec6
CLEPIL <f18121@g.ichinoseki.ac.jp>
2021-11-09 09:07:31 +0900

    プロフィールヘッダーvar.1

--- pages/account/_uid.vue
+++ pages/account/_uid.vue
@@ -1,13 +1,32 @@
 <template>
-  <v-container>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card class="py-4 d-flex justify-center">
-          {{ uid }}
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="header">
+    <v-container>
+      <v-row>
+        {{name}}
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card>
+            投稿<br>{{toukou}}
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            いったよ<br>{{ittayo}}
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            バッチ<br>{{batti}}
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-icon>mdi-map-marker</v-icon>{{location}}
+        {{bio}}
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -15,11 +34,22 @@ export default {
   data () {
     return {
       uid: this.$route.params.uid,
+      dialog: false,
+      location: '日本',
+      toukou: '555',
+      ittayo: '555',
+      batti: '555',
+      name: 'josei',
+      bio: 'わあああああああああああああああ',
     }
   },
 }
 </script>
 
 <style>
+.header {
+  background:#00EBC7;
+  border-radius: 0px 0px 16px 16px;
+}
 
 </style>
