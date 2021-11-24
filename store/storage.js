98771b47d236b08497577080578119962fb18eff
takuma-ru <kcattdev@gmail.com>
2021-11-24 13:58:40 +0900

    [Add] comment

--- store/storage.js
+++ store/storage.js
@@ -11,6 +11,7 @@ export const mutations = {
 }
 
 export const actions = {
+  // storageに保存されている画像のURLを取得する
   async getFileUrl (imgNmae) {
     const storageRef = this.$fire.storage.ref('postImages').child(`${imgNmae}`)
     try {
