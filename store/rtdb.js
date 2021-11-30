6abdbd252539abb69dd957b028645031ef1b8cbc
citron192 <jackatlas185@yahoo.co.jp>
2021-11-30 11:13:41 +0900

    修正

--- store/rtdb.js
+++ store/rtdb.js
@@ -1,11 +1,10 @@
 // rtdb/変数名import firebase from '@/plugins/firebase'
-import { v4 as uuidv4 } from 'uuid'
-
 export const state = () => ({
   PostData: [],
   Userpost: [],
   // userid: 'wirA5GIggboAFRoMMAkjIStPaAY5',
-  postdataId: 'a0eebc999c0b4ef8bb6d6bb9bd380a11',
+  postdataId: '',
+  // 今のところ投稿データごとには出来ないだよねー
   like: false,
   went: false,
 })
@@ -40,6 +39,9 @@ export const mutations = {
     }
   },
   userget (state, payload) {
+    if (state.Userpost != null) {
+      state.Userpost = null
+    }
     Object.keys(payload).forEach((val, key) => {
       payload[val].id = val
       state.Userpost.push(payload[val])
@@ -64,10 +66,6 @@ export const mutations = {
 export const actions = {
   async updataPostData ({ rootState }, { detail, tags, imgCoordinate, imgName }) {
     const updataPostdataRef = this.$fire.database.ref('posts')
-    // eslint-disable-next-line no-unused-vars
-    const uuid = uuidv4()
-    console.log(uuid)
-
     try {
       await updataPostdataRef.push({
         uid: rootState.auth.googleUserData.uid,
@@ -77,12 +75,13 @@ export const actions = {
         imgName,
         likesSum: 0,
         wentSum: 0,
-        date: 1634863170,
+        date: new Date().getTime(),
       })
     } catch (e) {
       alert(e)
     }
   },
+  // 修正後でしないとね
   async likesum ({ state, commit }) {
     // const likesumRef = this.$fire.datadase.ref('posts')
     const likesumRef = this.$fire.database.ref('posts/' + state.postdataId + '/likesSum')
@@ -103,7 +102,6 @@ export const actions = {
     }
   },
   async wentsum ({ state, commit }) {
-    // const likesumRef = this.$fire.datadase.ref('posts')
     const wentsumRef = this.$fire.database.ref('posts/' + state.postdataId + '/wentSum')
     try {
       commit('wentsum')
@@ -121,7 +119,7 @@ export const actions = {
       alert(e)
     }
   },
-  async removePostData ({ commit }, { PostDataId }) {
+  async removePostData ({ state, commit }, { PostDataId }) {
     const removePostDataRef = this.$fire.database.ref('posts')
     try {
       await removePostDataRef.child(PostDataId).remove()
@@ -136,7 +134,7 @@ export const actions = {
     const getpostdataRef = this.$fire.database.ref('posts')
     try {
       if (state.PostData.length) {
-        await getpostdataRef.orderByKey().startAt(state.postdataId).limitToFirst(2).once('value', (snapshot) => {
+        await getpostdataRef.orderByKey().startAt(state.postdataId).limitToFirst(11).once('value', (snapshot) => {
           console.log(snapshot.val())
           commit('addPostDataID', snapshot.val())
         })
@@ -150,7 +148,7 @@ export const actions = {
       alert(e)
     }
   },
-  async userPostData ({ commit }, { uid }) {
+  async getuserPostData ({ commit }, { uid }) {
     console.log(uid)
     const userpostRef = this.$fire.database.ref('posts')
     try {
@@ -163,5 +161,4 @@ export const actions = {
       alert(e)
     }
   },
-
 }
