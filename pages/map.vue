9f7c4b0da6f8045c00a339c3500210080ee1baeb
Ayuu <ayuu.kumakuma@gmail.com>
2021-12-04 00:08:48 +0900

    複数マーカー微調整

--- pages/map.vue
+++ pages/map.vue
@@ -15,6 +15,9 @@
             type="lg_sq"
             @click.native="getLocation()"
           />
+          <v-card style="position: absolute; z-index: 500;">
+            {{ zoom }}
+          </v-card>
           <l-tile-layer
             :url="`https://cartodb-basemaps-{s}.global.ssl.fastly.net/${$vuetify.theme.dark ? 'dark' : 'light'}_all/{z}/{x}/{y}.png`"
             :attribution="attribution"
@@ -23,10 +26,11 @@
             :lat-lng="center"
             :icon="icon"
           />
+          <!--一定のズーム値になるまではマーカー非表示とかのほうがいいかも-->
           <l-marker
             v-for="marker in markers"
             :key="marker.id"
-            :lat-lng.sync="marker.position"
+            :lat-lng="marker.position"
             :icon="spotIcon"
           />
         </l-map>
@@ -37,13 +41,14 @@
 <script>
 import L from 'leaflet'
 import iconImage from '../static/icon/fascino_logo_noback.svg'
-import spoticonImage from '../static/icon/map-marker-star.svg'
+import spoticonImage from '../static/icon/map-marker.svg'
 export default {
   name: 'Map',
   components: {
   },
   data () {
     return {
+      expand: false,
       zoom: 17,
       center: [0, 0],
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -58,25 +63,25 @@ export default {
       }),
       spotIcon: L.icon({
         iconUrl: spoticonImage,
-        iconsize: [48, 48],
-        iconAnchor: [24, 24],
+        iconsize: [60, 60],
+        iconAnchor: [30, 30],
       }),
       markers: [
         {
           id: 'm1',
-          position: { lat: 39.42, lng: 141.09 },
+          position: { lat: 38.919609806206346, lng: 141.1240648198379 },
         },
         {
           id: 'm2',
-          position: { lat: 51.8905, lng: -0.09 },
+          position: { lat: 38.92480770805559, lng: 141.10752110839584 },
         },
         {
           id: 'm3',
-          position: { lat: 51.005, lng: -0.09 },
+          position: { lat: 38.93283557272672, lng: 141.10622267771873 },
         },
         {
           id: 'm4',
-          position: { lat: 50.7605, lng: -0.09 },
+          position: { lat: 38.935654177411884, lng: 141.13602725401006 },
         },
       ],
     }
